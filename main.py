72a9cf096d4c9932d802dd4a26225132c2aea459
Mikko Pihlanen <pihlanem@student.tut.fi>
2018-06-17 14:06:10 +0300

    Small fix

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ while True:
     elif pool.floatSwitch.get_state() == 1:
 
         if pool.get_state() == "FOFF":
+            pool.get_temperatures()
             continue
 
         elif pool.get_state() == "ON":
