d98003d738d530f66d63a802d9967a40918250a6
Mikko Pihlanen <pihlanem@student.tut.fi>
2018-11-25 14:33:34 +0200

    Two views for display.

--- display.py
+++ display.py
@@ -12,6 +12,8 @@ class DisplayThread(threading.Thread):
         self.lcd.set_backlight(True)
         self.data_q = out_display_q
         self.data = 0
+        self.lastChange = int(time.time())
+        self.view = 0
 
     def draw(self):
         try:
@@ -21,30 +23,51 @@ class DisplayThread(threading.Thread):
         except:
             print("Can't get the data from queue. (display)")
 
-        self.lcd.set_cursor(row=0, col=0)
-        #self.lcd.message(datetime.now().strftime('%H:%M'))  # HH:MM
-        self.lcd.message(str(self.data["water_level"])+ "  ")
+        if self.lastChange + 5 <= int(time.time()):
 
-        self.lcd.set_cursor(row=0, col=10)
-        # Temperature formatted to one decimal
-        self.lcd.message(format(self.data["temp_high"],
-                                '.1f') + chr(223) + "C ")
+            self.lastChange = int(time.time())
+            self.lcd.clear()
 
-       # self.lcd.set_cursor(row=1, col=0)
-       # self.lcd.message("->" + datetime.fromtimestamp(
-       #     int(self.data["estimate"])).strftime('%H:%M '))  # HH:MM
+            if self.view == 0:
+                self.view = 1
+            else:
+                self.view = 0
 
-        self.lcd.set_cursor(row=1, col=0)
-        self.lcd.message(self.data["warming_phase"] + "    ")
+        elif self.view == 0:
 
-        self.lcd.set_cursor(row=1, col=10)
-        self.lcd.message(format(self.data["target"], '.1f') + chr(223) + "C ")
+            self.lcd.set_cursor(row=0, col=0)
+            self.lcd.message(datetime.now().strftime('%H:%M'))  # HH:MM
+
+            self.lcd.set_cursor(row=0, col=11)
+            self.lcd.message(self.data["warming_phase"] + "  ")
+
+            self.lcd.set_cursor(row=1, col=0)
+            self.lcd.message(format(self.data["temp_high"],
+                                    '.1f') + chr(223) + "C ")
+
+            self.lcd.set_cursor(row=1, col=10)
+            self.lcd.message(
+                format(self.data["target"], '.1f') + chr(223) + "C ")
+
+        elif self.view == 1:
+
+            self.lcd.set_cursor(row=0, col=0)
+            self.lcd.message(datetime.now().strftime('%H:%M'))  # HH:MM
+
+            self.lcd.set_cursor(row=0, col=11)
+            self.lcd.message(self.data["warming_phase"] + "  ")
+
+            self.lcd.set_cursor(row=1, col=0)
+            self.lcd.message(str(self.data["water_level"]) + "cm")
+
+            self.lcd.set_cursor(row=1, col=10)
+            self.lcd.message(str(self.data["water_level_target"]) + "cm")
 
     def run(self):
 
         self.lcd.clear()
         self.lcd.set_cursor(row=0, col=0)
-        self.lcd.message("PaljuPi Beta")
+        self.lcd.message("PaljuPi RC-1")
         time.sleep(5)
         self.lcd.clear()
 
--- pressureSender.py
+++ pressureSender.py
@@ -6,6 +6,7 @@ class PressureSender:
         self.water_level = 0
         self.set = False
         self.ser = -1
+        self.water_density = 997  # default
 
     def get_state(self):
         return self.set
@@ -24,6 +25,7 @@ class PressureSender:
                 return self.water_level
 
         offset = 5
+
         try:
             value = float(self.ser.readline().decode().strip('\r\n'))
             if value == 0:
@@ -35,13 +37,15 @@ class PressureSender:
             value = 0.5
             offset = 0
 
-        # Water's density is calculated with Kell's formula, should be valid for 0-150'C
+        elif(water_temperature != -85):
 
-        water_density = (999.83952+16.945176*water_temperature-(7.987040*10**-3)*water_temperature**2-(46.170461*10**-6)*water_temperature**3 +
-                         (105.56302*10**-9)*water_temperature**4-(280.54253*10**-12)*water_temperature**5)/(1+16.897850*10**-3*water_temperature)
+            # Water's density is calculated with Kell's formula, should be valid for 0-150'C
+
+            self.water_density = (999.83952+16.945176*water_temperature-(7.987040*10**-3)*water_temperature**2-(46.170461*10**-6)*water_temperature**3 +
+                                  (105.56302*10**-9)*water_temperature**4-(280.54253*10**-12)*water_temperature**5)/(1+16.897850*10**-3*water_temperature)
 
         psi = 5*(value-0.5)/(4.5-0.5)
         pa = psi*6894.76  # 1 psi is 6894.76 pascals
-        h_cm = 100*(pa / (water_density*9.81)) + offset
+        h_cm = 100*(pa / (self.water_density*9.81)) + offset
         self.water_level = format(h_cm, '.1f')
         return self.water_level
