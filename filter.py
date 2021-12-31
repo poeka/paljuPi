6810e17d56fe58654d3b80d846428ee6c72ba0ca
Mikko Pihlanen <mikko.pihlanen@gmail.com>
2021-12-31 14:11:12 +0200

    Changed GPIO pin for filter control

--- filter.py
+++ filter.py
@@ -3,13 +3,13 @@ import RPi.GPIO as GPIO
 
 class Filter:
     def __init__(self):
-        GPIO.setup(18, GPIO.OUT)
-        GPIO.output(18, GPIO.HIGH)
+        GPIO.setup(15, GPIO.OUT)
+        GPIO.output(15, GPIO.HIGH)
 
     def toggle_on(self):
-        GPIO.output(18, GPIO.LOW)  # LOW is ON
+        GPIO.output(15, GPIO.LOW)  # LOW is ON
         return
 
     def toggle_off(self):
-        GPIO.output(18, GPIO.HIGH)  # HIGH is OFF
+        GPIO.output(15, GPIO.HIGH)  # HIGH is OFF
         return
